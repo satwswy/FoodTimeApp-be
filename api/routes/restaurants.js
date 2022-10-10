f71067e38191d271110f07befaea3d02d85a7518
marios <mgeorgak859@gmail.com>
2022-10-11 00:51:52 +0300

    adds error handler

--- api/routes/restaurants.js
+++ api/routes/restaurants.js
@@ -50,7 +50,7 @@ router.get("/", async (req, res) => {
     const restaurants = await Restaurant.find();
     res.status(200).json(restaurants);
   } catch (error) {
-    res.status(500).json(error);
+    next(error)
   }
 });
 
