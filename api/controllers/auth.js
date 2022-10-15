e010d79fb2873da74efa3e878028d0e7297c5c11
marios <mgeorgak859@gmail.com>
2022-10-15 20:09:37 +0300

    Update auth.js

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -9,8 +9,7 @@ export const register = async (req, res, next) => {
     const hash = bcrypt.hashSync(req.body.password, salt);
 
     const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
+      ...req.body,
       password: hash,
     });
 
@@ -40,7 +39,7 @@ export const login = async (req, res, next) => {
 
     const { password, isAdmin, ...otherDetails } = user._doc;
 
-    res.cookie("access_token", token,{httpOnly:true}).status(200).json({ ...otherDetails });
+    res.cookie("access_token", token,{httpOnly:true}).status(200).json({ details: { ...otherDetails }, isAdmin });
   } catch (error) {
     next(error);
   }
