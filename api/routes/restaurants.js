13dd325d7c8dec1d6c1735cdb988447281bd2a44
marios <mgeorgak859@gmail.com>
2022-10-15 20:39:45 +0300

    Update restaurants.js

--- api/routes/restaurants.js
+++ api/routes/restaurants.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { countByCity, countByType, createRestaurant, deleteRestaurant, getRestaurant, getRestaurants, updateRestaurant } from "../controllers/restaurant.js";
+import { countByCity, countByType, createRestaurant, deleteRestaurant, getRestaurant, getRestaurants, getRestaurantTables, updateRestaurant } from "../controllers/restaurant.js";
 import { verifyToken, verifyUser, verifyAdmin } from "../utils/verifyToken.js";
 import Restaurant from "../models/Restaurant.js";
 import { createError } from "../utils/error.js";
@@ -18,5 +18,6 @@ router.get("/", getRestaurants);
 
 router.get("/countByCity", countByCity);
 router.get("/countByType", countByType);
+router.get("/table/:id", getRestaurantTables);
 
 export default router;
