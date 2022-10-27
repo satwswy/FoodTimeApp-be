95df14e89437b060fdcf283c6869a71b84cd2cb6
marios <mgeorgak859@gmail.com>
2022-10-27 10:48:41 +0300

    Update Restaurant.js

--- api/models/Restaurant.js
+++ api/models/Restaurant.js
@@ -2,16 +2,12 @@ import mongoose from "mongoose";
 
 const RestaurantSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  type: { type: String, required: true },
+  type: { type: [String], required: true },
   city: { type: String, required: true },
   address: { type: String, required: true },
-  distance: { type: String, required: true },
   photos: { type: [String] },
-  title: { type: String, required: true },
   desc: { type: String, required: true },
-  rating: { type: Number, min: 0, max: 5 },
   tables: { type: [String] },
-  featured: { type: Boolean, default: false },
 });
 
 export default mongoose.model("Restaurant", RestaurantSchema);
