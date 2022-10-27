bfe6d40d272d687774707cf13cf464f197e18eb4
marios <mgeorgak859@gmail.com>
2022-10-28 00:23:55 +0300

    Update auth.js

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -42,7 +42,7 @@ export const login = async (req, res, next) => {
     res
       .cookie("access_token", token, { httpOnly: true })
       .status(200)
-      .json({ details: { ...otherDetails }, isAdmin });
+      .json({ details: { ...otherDetails , isAdmin } });
   } catch (error) {
     next(error);
   }
