dac8ec03409542aa8a2b60fa77f53b16d1d56232
marios <mgeorgak859@gmail.com>
2022-10-29 23:12:38 +0300

    Update restaurant.js

--- api/controllers/restaurant.js
+++ api/controllers/restaurant.js
@@ -1,92 +1,141 @@
 import Restaurant from "../models/Restaurant.js";
 import Table from "../models/Table.js";
 
-export const createRestaurant = async (req,res,next)=>{
-    const newRestaurant = new Restaurant(req.body);
-
-    try {
-      const savedRestaurant = await newRestaurant.save();
-      res.status(200).json(savedRestaurant);
-    } catch (error) {
-      next(error)
-    }
-}
-
-export const updateRestaurant = async (req,res,next)=>{
-    try {
-        const updatedRestaurant = await Restaurant.findByIdAndUpdate(
-          req.params.id,
-          { $set: req.body },
-          { new: true }
-        );
-        res.status(200).json(updatedRestaurant);
-      } catch (error) {
-        next(error)
-      }
-}
-
-export const deleteRestaurant = async (req,res,next)=>{
-    try {
-        await Restaurant.findByIdAndDelete(req.params.id);
-        res.status(200).json("Restaurant has been deleted");
-      } catch (error) {
-        next(error)
-      }
-}
-
-export const getRestaurant = async (req,res,next)=>{
-    try {
-        const restaurant = await Restaurant.findById(req.params.id);
-        res.status(200).json(restaurant);
-      } catch (error) {
-        next(error);
-      }
-}
-
-export const getRestaurants = async (req,res,next)=>{
-  const {...queries} = req.query;
-    try {
-        const restaurants = await Restaurant.find({
-          ...queries
-        });
-        res.status(200).json(restaurants);
-      } catch (error) {
-        next(error)
-      }
-}
-
-export const countByCity = async (req,res,next)=>{
-  const cities = req.query.cities.split(",")
-  try {
-      const list = await Promise.all(cities.map(city=>{
-        return Restaurant.countDocuments({city:city})
-      }))
-      res.status(200).json(list);
-    } catch (error) {
-      next(error)
-    }
-}
-
-export const countByType = async (req,res,next)=>{
+export const createRestaurant = async (req, res, next) => {
+  const newRestaurant = new Restaurant(req.body);
 
   try {
-    const pizzaCount = await Restaurant.countDocuments({type:"pizza"})
-    const burgersCount = await Restaurant.countDocuments({type:"burgers"})
-    const pastaCount = await Restaurant.countDocuments({type:"pasta"})
-    const grillCount = await Restaurant.countDocuments({type:"grill"})
-    const seaFoodCount = await Restaurant.countDocuments({type:"seeFood"})
+    const savedRestaurant = await newRestaurant.save();
+    res.status(200).json(savedRestaurant);
+  } catch (error) {
+    next(error);
+  }
+};
 
-      res.status(200).json([
-        {type:"pizza",count:pizzaCount},
-        {type:"burgers",count:burgersCount},
-        {type:"pasta",count:pastaCount},
-        {type:"grill",count:grillCount},
-        {type:"seaFood",count:seaFoodCount},
-      ]);
-    } catch (error) {
-      next(error)
+export const updateRestaurant = async (req, res, next) => {
+  try {
+    const updatedRestaurant = await Restaurant.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedRestaurant);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteRestaurant = async (req, res, next) => {
+  try {
+    await Restaurant.findByIdAndDelete(req.params.id);
+    res.status(200).json("Restaurant has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getRestaurant = async (req, res, next) => {
+  try {
+    const restaurant = await Restaurant.findById(req.params.id);
+    res.status(200).json(restaurant);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getRestaurants = async (req, res, next) => {
+  const { ...queries } = req.query;
+  try {
+    const restaurants = await Restaurant.find();
+    if ( req.query.type && req.query.city) {
+      const typesList = req.query.type.split(",");
+      const filteredRestaurantsWithCity = []
+      const filteredRestaurants = [] 
+      restaurants.map(restaurant=> {
+        let foodList=[]
+        restaurant.type.map(current =>{
+         let food = typesList.includes(current)
+         foodList.push(food)
+        })
+        console.log(foodList)
+        if(foodList.includes(true)){
+          filteredRestaurants.push(restaurant)
+        }
+      }
+        )
+        filteredRestaurants.map(restau=>{
+          if(restau.city=== req.query.city){
+            filteredRestaurantsWithCity.push(restau)
+          }
+        })
+      res.status(200).json(filteredRestaurantsWithCity);
+    } else if(req.query.type) {
+      const typesList = req.query.type.split(",");
+      const filteredRestaurants = [] 
+      restaurants.map(restaurant=> {
+        let foodList=[]
+        restaurant.type.map(current =>{
+         let food = typesList.includes(current)
+         foodList.push(food)
+        })
+        console.log(foodList)
+        if(foodList.includes(true)){
+          filteredRestaurants.push(restaurant)
+        }
+      }
+        )
+      res.status(200).json(filteredRestaurants);
     }
-}
+    else if(req.query.city){
+      const filteredRestaurants = []
+      restaurants.map(restaur=>{
+        if(restaur.city===req.query.city){
+          filteredRestaurants.push(restaur)
+        }
+      })
+      res.status(200).json(filteredRestaurants);
+    }
+    else {
+      res.status(200).json(restaurants);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const countByCity = async (req, res, next) => {
+  const cities = req.query.cities.split(",");
+  try {
+    const list = await Promise.all(
+      cities.map((city) => {
+        return Restaurant.countDocuments({ city: city });
+      })
+    );
+    res.status(200).json(list);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const countByType = async (req, res, next) => {
+  try {
+    const pizzaCount = await Restaurant.countDocuments({ type: "pizza" });
+    const burgersCount = await Restaurant.countDocuments({ type: "burgers" });
+    const pastaCount = await Restaurant.countDocuments({ type: "pasta" });
+    const grillCount = await Restaurant.countDocuments({ type: "grill" });
+    const seaFoodCount = await Restaurant.countDocuments({ type: "seeFood" });
+
+    res.status(200).json([
+      { type: "pizza", count: pizzaCount },
+      { type: "burgers", count: burgersCount },
+      { type: "pasta", count: pastaCount },
+      { type: "grill", count: grillCount },
+      { type: "seaFood", count: seaFoodCount },
+    ]);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const getRestaurantTables = async (req, res, next) => {
   try {
@@ -96,8 +145,8 @@ export const getRestaurantTables = async (req, res, next) => {
         return Table.findById(table);
       })
     );
-    console.log('LIST', list)
-    res.status(200).json(list)
+    console.log("LIST", list);
+    res.status(200).json(list);
   } catch (err) {
     next(err);
   }
@@ -116,15 +165,14 @@ export const getReservations = async (req, res, next) => {
     );
     // const reservations = await Table.find(); // TODO: don't grab all the tables, but just the ones
     // for the restaurant you're passing in the query param
-    let array = []
-    list.forEach(table =>{
-      table.tableNumbers.forEach(tableNumber=>{
-        if(tableNumber.unavailableDates.length >0){
-          array.push(tableNumber)
+    let array = [];
+    list.forEach((table) => {
+      table.tableNumbers.forEach((tableNumber) => {
+        if (tableNumber.unavailableDates.length > 0) {
+          array.push(tableNumber);
         }
-        
-      })
-    })
+      });
+    });
     res.status(200).json(array);
   } catch (error) {
     next(error);
